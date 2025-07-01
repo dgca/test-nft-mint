217dd556ac590d19f7427be07a69af23fcca8833
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-07-01 13:47:27 -0600

    Add allowed addresses

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -39,5 +39,8 @@ export async function GET() {
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
     }),
+    baseBuilder: {
+      allowedAddresses: ["0xfa6b3dF826636Eb76E23C1Ee38180dB3b8f60a86"],
+    },
   });
 }
